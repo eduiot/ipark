82d77093ef088292be30a04093ade5ec53ab11eb
Atikur Rabbi <msawon94@gmail.com>
2019-06-24 13:18:02 +0400

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady () {
+
+    //StatusBar.hide();
+/*
+    cordova.plugins.notification.local.schedule([
+    { id: 1, title: 'My first notification' },
+    { id: 2, title: 'My first notification' }
+    ]);
+
+    */
+
+}
+
